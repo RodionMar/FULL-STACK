361e733ca2120ea9a7a07235948f391fb6f04b2d
RodionMar <rodionmarmura@gmail.com>
2023-07-27 10:12:19 +0300

    feat: add Dockerfile and change docker-compose

--- server/backend/src/app.module.ts
+++ server/backend/src/app.module.ts
@@ -1,35 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './user/user.module';
-import { ListModule } from './list/list.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from './config.module';
+import { UserModule } from '@entities/user/user.module';
+import { TypeOrmModule } from '@db/typeorm.module';
 
 @Module({
   imports: [
+    ConfigModule,
     UserModule,
-    ListModule,
-    ConfigModule.forRoot({isGlobal: true, envFilePath: '../.env'}),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (config: ConfigService) => ({
-        type: config.get<'postgres'>('TYPEORM_CONNECTION'),
-        username: config.get<string>('TYPEORM_USERNAME'),
-        password: config.get<string>('TYPEORM_PASSWORD'),
-        database: config.get<string>('TYPEORM_DATABASE'),
-        host: config.get<string>('TYPEORM_HOST'),
-        port: config.get<number>('TYPEORM_PORT'),
-        entities: [__dirname + 'dist/**/*.entity{.ts, .js}'],
-        synchronize: true,
-        // autoLoadEntities: true,
-        logging: true
-      })
-    }),
+    TypeOrmModule
   ],
-  providers: [AppService],
-  controllers: [AppController],
 })
 
 export class AppModule {}
--- server/backend/src/main.ts
+++ server/backend/src/main.ts
@@ -1,16 +1,15 @@
-import { ConfigService } from '@nestjs/config';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+// import { ConfigService } from '@nestjs/config'
+
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
-  const PORT = process.env.API_PORT || 5000;
-  app.enableCors();
-  // const config = app.get(ConfigService);
-  // const port = config.get<number>('API_PORT');
-  await app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`)
-  })
+	const app = await NestFactory.create(AppModule)
+	// const config = app.get(ConfigService)
+	// const port = config.get<number>('API_PORT')
+	await app.listen(3000, () => {
+		console.log(`App started on port: ${3000}`)
+	})
 }
-bootstrap();
+
+bootstrap()
